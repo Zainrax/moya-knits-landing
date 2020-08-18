5a8aee528d4e4e9703718ed64a48c8e9f291fe1b
zainrax <patrickb321@gmail.com>
2020-08-18 14:20:54 +1200

    fix(signup): remove split

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -10,7 +10,7 @@ export default async (req, res) => {
   try {
     const LIST_ID = process.env.MAILCHIMP_LIST_ID;
     const API_KEY = process.env.MAILCHIMP_API_KEY;
-    const DATACENTER = API_KEY.split("-")[1];
+    const DATACENTER = "us10";
     const data = {
       email_address: email,
       status: "subscribed",
